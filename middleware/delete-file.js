b02717c3349776761a940c3945845505ce4f0c9e
Bladimir <oirbl@hotmail.com>
2020-08-06 23:39:00 -0500

    change path to folder

--- middleware/delete-file.js
+++ middleware/delete-file.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = async (req, res, next) => {
 
     try {
-        const FOLDER_TO_REMOVE = 'images/tempImg';
+        const FOLDER_TO_REMOVE = '/tempImg';
         await fs.readdir(FOLDER_TO_REMOVE).then(files => {
             if (files.length === 0) return;
             const unlinkPromises = files.map(file => {
@@ -22,6 +22,7 @@ module.exports = async (req, res, next) => {
     } catch (error) {
         console.log('error:')
         console.log(error);
+        next();
     }
 
 }
