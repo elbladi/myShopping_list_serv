96477cfcaa44b2566bc5090d31ee4bdff112d972
Bladimir <oirbl@hotmail.com>
2020-08-06 23:04:26 -0500

    fix image types

--- middleware/delete-file.js
+++ middleware/delete-file.js
@@ -2,11 +2,11 @@ const fs = require('fs').promises
 const path = require('path')
 
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 
     try {
         const FOLDER_TO_REMOVE = 'images/tempImg';
-        fs.readdir(FOLDER_TO_REMOVE).then(files => {
+        await fs.readdir(FOLDER_TO_REMOVE).then(files => {
             const unlinkPromises = files.map(file => {
                 const filePath = path.join(FOLDER_TO_REMOVE, file)
                 return fs.unlink(filePath);
